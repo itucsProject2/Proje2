1fe7d271c4ab1d7f0b1221aaecd10eb40a2ccea6
oykubozkir <oykubozkir@gmail.com>
2017-04-21 11:06:10 +0300

    verify token ayarlandı

--- botair/views.py
+++ botair/views.py
@@ -4,4 +4,7 @@ from django.http.response import HttpResponse
 
 class BotairView(generic.View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
+        if self.request.GET['hub.verify_token'] == 'EAAJPGyHraTUBAIiXMqZCZBtmZACxAmxQ9YZB9BZBCQao67vZADEfd2QMHiZBQDIHX621SFbEGRyIpdTxQIaiZBNskRiWg2r9mBap5jfjA6aOypHL4cHSe01vddzzEe8TFimUzGSF2WpCUWvlEgkItA5lFPan0iLozsMcH7pyokLaZCwZDZD':
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
