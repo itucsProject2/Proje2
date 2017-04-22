d8b9507a3c1b0de1cd500bec0e5c9cbdfc47e42f
oykubozkir <oykubozkir@gmail.com>
2017-04-22 18:47:11 +0300

    bugfix

--- botair/witOperations.py
+++ botair/witOperations.py
@@ -2,6 +2,7 @@ from wit import Wit
 
 def sendToWit(messageString):
     access_token = 'DJE4HFOBMAJO6DMIC2IEZRP5DDRQRZKS'
+    actions ={}
     client = Wit(access_token = access_token,actions = actions)
     response = client.message(messageString);
     print(response)
